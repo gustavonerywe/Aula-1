22ecd4e73ad549b4307d9ff3fec1ad897c4ebcce
gustavonerywe <90864370+gustavonerywe@users.noreply.github.com>
2022-06-08 19:09:14 -0300

    Add files via upload

--- pcweb/Lab1/js/app.js
+++ pcweb/Lab1/js/app.js
@@ -1,6 +1,11 @@
 const numberOne = document.getElementById("numberOne");
 const btn = document.getElementById("botao");
 const btnn = document.getElementById("btn");
+const btn1 = document.getElementById("botao1");
+const numberTwo = document.getElementById("numberTwo")
+const getSum = document.getElementById("getSum");
+const btn2 = document.getElementById("botao2");
+const numberThree = document.getElementById("numberThree")
 
 const fatorial = (numberOne) =>{
     let result = numberOne
@@ -9,14 +14,33 @@ const fatorial = (numberOne) =>{
     }
     return result
 }
-const pares = ()=>{
-    let num = 1000;
-    for(let i = 1; i < num; i++){
-        if(i % 2 == 0){
-            return i +;
+
+const potencia = (numberTwo) =>{
+    let resultOfPow = numberTwo
+    for(let i = 1; i <= 30; i++){
+        resultOfPow =  Math.pow(numberTwo, i)
+    }
+    return resultOfPow;
+}
+
+const pares = (getSum) =>{
+    let num = getSum;
+    let aux;
+    for(let i = 1; i<=num;i++){
+        if(i % 2 == 0) {
+            aux = aux++;
         }
     }
+    return aux;
 }
+
+function fibo(numberThree){
+    if (numberThree === 1) return 1;
+    if (numberThree === 2) return 2;
+    fibo(numberThree - 1) + fibo(numberThree - 2);
+    return fibo;
+}
+
 btn.addEventListener("click", ()=>{
     const showFatorial=document.getElementById("fatorial");
     showFatorial.innerHTML = fatorial(numberOne.value);
@@ -24,5 +48,13 @@ btn.addEventListener("click", ()=>{
 
 btnn.addEventListener("click", ()=>{
     const showPares=document.getElementById("pares");
-    showPares.innerHTML = pares();
+    showPares.innerHTML = pares(getSum.value);
+})
+btn1.addEventListener("click", ()=>{
+    const showPow=document.getElementById("showPow");
+    showPow.innerHTML = potencia(numberTwo.value)
+})
+btn2.addEventListener("click", ()=>{
+    const showFibo=document.getElementById("showFibo");
+    showFibo.innerHTML = fibo(numberThree)
 })
--- pcweb/aula_05/js/app.js
+++ pcweb/aula_05/js/app.js
@@ -1,2 +1,14 @@
 const btn=document.getElementById("submit");
 const birthday=document.getElementById("birthday");
+const timeLived=document.getElementById("timeLived");
+
+const hoursLived = (timeLived)=>{
+    let today = new Date();
+    timeLived = new Date(birthday);
+    let diff = today - timeLived
+    return diff;
+}
+
+btn.addEventListener("click", ()=>{
+    timeLived.innerHTML = hoursLived(birthday.value)  
+})
