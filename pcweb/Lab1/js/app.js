e442f299788d569ad6290f34b4aeda5f9b781385
gustavonerywe <90864370+gustavonerywe@users.noreply.github.com>
2022-06-13 20:21:21 -0300

    Add files via upload

--- pcweb/Lab1/js/app.js
+++ pcweb/Lab1/js/app.js
@@ -6,6 +6,8 @@ const numberTwo = document.getElementById("numberTwo")
 const getSum = document.getElementById("getSum");
 const btn2 = document.getElementById("botao2");
 const numberThree = document.getElementById("numberThree")
+const btn3 = document.getElementById("justBtn");
+const getPrimo = document.getElementById("getPrimo");
 
 const fatorial = (numberOne) =>{
     let result = numberOne
@@ -22,24 +24,41 @@ const potencia = (numberTwo) =>{
     }
     return resultOfPow;
 }
-
-const pares = (getSum) =>{
-    let num = getSum;
-    let aux;
-    for(let i = 1; i<=num;i++){
-        if(i % 2 == 0) {
-            aux = aux++;
+const pares = (getsum) =>{
+    let soma = 0
+    for(let i = 1; i <=1000; i++){
+        if(i % 2 == 0){
+          soma += i;
         }
     }
-    return aux;
+    return soma;
 }
 
-function fibo(numberThree){
-    if (numberThree === 1) return 1;
-    if (numberThree === 2) return 2;
-    fibo(numberThree - 1) + fibo(numberThree - 2);
-    return fibo;
+const fibo = (numberThree)=>{
+    let sum = 0
+    let beforeNumber = 0
+    let afterNumber = 1
+    for(let i = 0; i < numberThree; i++){
+        sum = beforeNumber + afterNumber;
+        beforeNumber = afterNumber;
+        afterNumber = sum;
+    }
+    return beforeNumber;
 }
+const justPrime = (getPrimo)=>{
+    for(let i = 2; i <= getPrimo; i++) {
+        if(justPrime2(i));
+    } 
+   return getPrimo;
+}
+const justPrime2 = (i) =>{
+        for(let div = 2; div < i; div++){
+            if(i % div === 0){
+                return false;
+            }
+        }
+       return true;
+    }
 
 btn.addEventListener("click", ()=>{
     const showFatorial=document.getElementById("fatorial");
@@ -56,5 +75,9 @@ btn1.addEventListener("click", ()=>{
 })
 btn2.addEventListener("click", ()=>{
     const showFibo=document.getElementById("showFibo");
-    showFibo.innerHTML = fibo(numberThree)
+    showFibo.innerHTML = fibo(numberThree.value)
+})
+btn3.addEventListener("click", ()=>{
+    const showPrimo=document.getElementById("showPrimo");
+    showPrimo.innerHTML = justPrime(getPrimo.value)
 })
