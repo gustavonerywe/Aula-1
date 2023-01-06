4037eaf06ff2dc881f5b2d06c3a521bfc8381b9b
gustavonerywe <90864370+gustavonerywe@users.noreply.github.com>
2023-01-06 12:59:42 -0300

    Add files via upload

--- gugu/crud-filmes/controller/filmeInserir.php
+++ gugu/crud-filmes/controller/filmeInserir.php
@@ -1,3 +1,29 @@
 ﻿<?php
-  
-?>
+    require_once ('../model/conexao.php');
+    $filmePost = file_get_contents('php://input');
+    $filmeMatriz = json_decode($filmePost, true);
+    $titulo = (isset($filmeMatriz["titulo"]) && $filmeMatriz["titulo"] != null) ? strtoupper($filmeMatriz["titulo"]) : "";
+    $avaliacao = (isset($filmeMatriz["avaliacao"]) && $filmeMatriz["avaliacao"] != null) ? $filmeMatriz["avaliacao"] : "";
+    $generoId = (isset($filmeMatriz["genero_id"]) && $filmeMatriz["genero_id"] > 0) ? $filmeMatriz["genero_id"] : "";
+
+    $resposta["erro"] = false;
+    $resposta["dados"] = null;
+    $resposta["msgErro"] = '';
+    $resposta["msgSucesso"] = '';
+
+    if($titulo != "" && $avaliacao != "") {
+        try {
+            $sql = "INSERT INTO filmes_assistidos(titulo, avaliacao, genero_id) VALUES(?,?,?)";
+            $stmt = $conexao -> prepare($sql);
+            $stmt -> bindParam(1, $titulo);
+            $stmt -> bindParam(2, $avaliacao);
+            $stmt -> bindParam(3, $generoId);
+            $stmt -> execute();
+            $resposta["msgSucesso"] = "{$stmt -> rowCount()} filme inserido com sucesso! O id inserido foi {$connection -> lastInsertId()}";
+        } catch (PDOException $e) {
+            $resposta["erro"] = true;
+            $resposta["msgErro"] = "Erro: Não foi possível efetuar a inserção no BD".$e -> getMessage();
+        } finally {
+            echo json_encode($resposta);
+        }
+    }
